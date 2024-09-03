2c12490ac4fc5be881103eefc6778de1b0602c2b
kapil <kc918062@gmail.com>
2024-09-03 12:50:06 +0530

    done

--- password_gerator2/src/App.jsx
+++ password_gerator2/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import { useState } from 'react'
 
 function App() {
@@ -7,6 +7,8 @@ function App() {
   let [charallowed, setcharallowed] = useState(false);
   let [password, setpassword] = useState();
 
+  //ref hooks
+  const passwordref = useRef(null);
 
   const radompassword = useCallback(() => {
     let pass = "";
@@ -25,6 +27,13 @@ function App() {
     setpassword(pass)
   }, [length, numberallowed, charallowed, setpassword])
 
+  const copypasswordToClipboard = useCallback(() => {
+    if (passwordref.current) {
+      passwordref.current.select();
+      window.navigator.clipboard.writeText(password);
+    }
+  }, [password]);
+
   useEffect(() => {
     radompassword()
   }, [length, numberallowed, charallowed, radompassword])
@@ -35,46 +44,55 @@ function App() {
       <div className='flex flex-col justify-center items-center h-screen bg-gray-800'>
         <h1 className='text-4xl text-white m-4'>Password Generator</h1>
         <div className='mt-6 overflow-hidden mb-4 shadow p-4 bg-gray-700 rounded-lg'>
-          <input
-            type="text"
-            placeholder='Password'
-            value={password}
-            readOnly
-            className='rounded-md p-2 w-full mb-4 bg-gray-900 text-white'
-          />
-          <label className='border rounded-md p-2 pl-2 pr-1 bg-white mb-auto'>length of Password: {length}</label>
+          <div className='flex flex-row'>
+            <input
+              type="text"
+              placeholder='Password'
+              value={password}
+              readOnly
+              ref={passwordref}
+              className='rounded-md p-2 w-full mb-4 bg-gray-900 text-white'
+            />
+            <button onClick={copypasswordToClipboard} className='border rounded-md p-2 pl-3 pr-3 m-2 mb-6 hover:bg-white'>Copy</button>
+          </div>
+          <label className='border rounded-md p-2 pl-2 pr-1 bg-white mb-auto'>
+            Length of Password: {length}
+          </label>
           <input
             type="range"
             min={5}
             max={100}
-            className='w-full mb-1 cursor-pointer mt-3 '
+            className='w-full mb-1 cursor-pointer mt-3'
             onChange={(e) => { setlength(e.target.value) }}
           />
-          <div className='flex items-center mb-4'>
-            <input
-              type="checkbox"
-              className='mr-2'
-              defaultChecked={numberallowed}
-              id='numberInput'
-              onChange={() => {
-                setnuberallowed((prev) => !prev)
-              }}
-            />
-            <label className='text-white'>Include Numbers</label>
-          </div>
-          <div className='flex items-center mb-4'>
-            <input
-              type="checkbox"
-              className='mr-2'
-              defaultChecked={charallowed}
-              id='charInput'
-              onChange={() => {
-                setcharallowed((prev) => !prev)
-              }}
-            />
-            <label className='text-white'>Include Special Characters</label>
-          </div>
         </div>
+
+        <div className='flex items-center mb-4'>
+          <input
+            type="checkbox"
+            className='mr-2'
+            defaultChecked={numberallowed}
+            id='numberInput'
+            onChange={() => {
+              setnuberallowed((prev) => !prev);
+            }}
+          />
+          <label className='text-white'>Include Numbers</label>
+        </div>
+
+        <div className='flex items-center mb-4'>
+          <input
+            type="checkbox"
+            className='mr-2'
+            defaultChecked={charallowed}
+            id='charInput'
+            onChange={() => {
+              setcharallowed((prev) => !prev);
+            }}
+          />
+          <label className='text-white'>Include Special Characters</label>
+        </div>
+
         <button
           onClick={radompassword}
           className='bg-blue-500 text-white px-4 py-2 rounded-md'
