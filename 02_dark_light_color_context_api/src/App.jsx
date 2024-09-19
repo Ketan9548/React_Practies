3599c70e5bddecd15f7b1522df002ac62e72656b
kapil <kc918062@gmail.com>
2024-09-19 10:48:11 +0530

    done

--- 02_dark_light_color_context_api/src/App.jsx
+++ 02_dark_light_color_context_api/src/App.jsx
@@ -1,18 +1,38 @@
+import { useEffect, useState } from 'react';
 import './App.css'
+import { ThemProvider } from './Context/Them';
+import Card from './Components/Card';
+import ButtonThem from './Components/ButtonThem';
 
 function App() {
+  const [themMode, setThememode] = useState("light");
+
+  const lightTheme = () => {
+    setThememode("light");
+  }
+  const darkTheme = () => {
+    setThememode("dark")
+  }
+  const value = document.querySelector('html').classList;
+  useEffect(() => {
+    value.remove("light", "dark");
+    value.add(themMode);
+  }, [themMode])
+
   return (
     <>
-      <div className='flex flex-wrap min-h-screen items-center'>
-        <div className='w-full'>
-          <div className='w-full max-x-sm mx-auto flex justify-end mb-4'>
-            name
-          </div>
-          <div className='w-full max-w-sm mx-auto'>
-            card
+      <ThemProvider value={{ themMode, lightTheme, darkTheme }}>
+        <div className='flex flex-wrap min-h-screen items-center'>
+          <div className='w-full'>
+            <div className='w-full max-w-sm mx-auto flex justify-end mb-4'>
+              <ButtonThem />
+            </div>
+            <div className='w-full max-w-sm mx-auto'>
+              <Card />
+            </div>
           </div>
         </div>
-      </div>
+      </ThemProvider >
     </>
   )
 }
