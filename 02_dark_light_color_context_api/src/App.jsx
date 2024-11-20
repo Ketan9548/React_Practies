f3c293d1c3c6ccbf826459c9f3ccbd3d48b4051d
Ketan9548 <kc918062@gmail.com>
2024-11-20 16:54:07 +0530

    done

--- 02_dark_light_color_context_api/src/App.jsx
+++ 02_dark_light_color_context_api/src/App.jsx
@@ -1,40 +1,42 @@
-import { useEffect, useState } from 'react';
-import './App.css'
-import { ThemProvider } from './Context/Them';
-import Card from './Components/Card';
-import ButtonThem from './Components/ButtonThem';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ThemProvider } from "./Context/Them";
+import Card from "./Components/Card";
+import ButtonThem from "./Components/ButtonThem";
 
 function App() {
   const [themMode, setThememode] = useState("light");
 
   const lightTheme = () => {
     setThememode("light");
-  }
+  };
   const darkTheme = () => {
-    setThememode("dark")
-  }
-  const value = document.querySelector('html').classList;
+    setThememode("dark");
+  };
+  const value = document.querySelector("html").classList;
   useEffect(() => {
     value.remove("light", "dark");
     value.add(themMode);
-  }, [themMode])
+  }, [themMode]);
 
   return (
     <>
-      <ThemProvider value={{ themMode, lightTheme, darkTheme }}>
-        <div className='flex flex-wrap min-h-screen items-center'>
-          <div className='w-full'>
-            <div className='w-full max-w-sm mx-auto flex justify-end mb-4'>
-              <ButtonThem />
-            </div>
-            <div className='w-full max-w-sm mx-auto'>
-              <Card />
+      <div className="bg-slate-500">
+        <ThemProvider value={{ themMode, lightTheme, darkTheme }}>
+          <div className="flex flex-wrap min-h-screen items-center">
+            <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                <ButtonThem />
+              </div>
+              <div className="w-full max-w-sm mx-auto">
+                <Card />
+              </div>
             </div>
           </div>
-        </div>
-      </ThemProvider >
+        </ThemProvider>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
