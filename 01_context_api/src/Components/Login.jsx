29802d463a07df8c058e60bdee78c36b0583d89c
Ketan9548 <kc918062@gmail.com>
2024-11-28 15:03:20 +0530

    done

--- 01_context_api/src/Components/Login.jsx
+++ 01_context_api/src/Components/Login.jsx
@@ -1,29 +1,38 @@
-import React, { useContext, useState } from 'react'
-import Contextapi from '../Context/Contextapi'
+import { useContext, useState } from "react";
+import Contextapi from "../Context/Contextapi";
 
 const Login = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const { setUser } = useContext(Contextapi)
+  const { setUser } = useContext(Contextapi);
 
-    const handelsubmit = (e) => {
-        e.preventDefault();
-        setUser({ username, password });
-    }
-    return (
-        <>
-            <div>
-                <h1>Login</h1>
-                <input type="text" placeholder='username' value={username} onChange={(e) => setUsername(e.target.value)} />
-                <br />
-                <br />
-                <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='password' />
-            </div>
-            <button onClick={handelsubmit}>Submit</button>
-        </>
-    )
-}
+  const handelsubmit = (e) => {
+    e.preventDefault();
+    setUser({ username, password });
+  };
+  return (
+    <>
+      <div>
+        <h1>Login</h1>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="password"
+        />
+      </div>
+      <button onClick={handelsubmit}>Submit</button>
+    </>
+  );
+};
 
-export default Login
-
+export default Login;
